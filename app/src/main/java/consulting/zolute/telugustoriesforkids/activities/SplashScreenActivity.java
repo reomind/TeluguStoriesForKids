db3b9365fe5c21cb83ae71625e783fa10ed3130e
Hussain Jawadwala <hussain@jawadwala.com>
2018-03-13 14:28:31 +0530

    fixed changes and added one signal

---
+++ app/src/main/java/consulting/zolute/telugustoriesforkids/activities/SplashScreenActivity.java
@@ -0,0 +1,15 @@
+package consulting.zolute.telugustoriesforkids.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import consulting.zolute.telugustoriesforkids.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
